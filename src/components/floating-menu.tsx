a855981420e88ef43dfe99dd79fac2e863cf26f6
Amritendu Bhattacharjee <34620625+xeonray-origin@users.noreply.github.com>
2024-02-21 16:55:14 +0530

    Project page and minor fixes, styling changes (#2)
    * feat: project cards
    * fix styling fixes and set max width to 2000px

--- src/components/floating-menu.tsx
+++ src/components/floating-menu.tsx
@@ -2,7 +2,15 @@
 
 import { useEffect, useState } from 'react'
 import { usePathname, useRouter } from 'next/navigation'
-import { AppWindow, Github, HomeIcon, PlusIcon } from 'lucide-react'
+import {
+	AppWindow,
+	Contact,
+	Github,
+	HomeIcon,
+	PlusIcon,
+	Rss,
+	Save,
+} from 'lucide-react'
 
 import {
 	IconButton,
@@ -10,12 +18,19 @@ import {
 	SpeedDialAction,
 	SpeedDialContent,
 	SpeedDialHandler,
+	Typography,
 } from './ui'
 
 const FloatingMenu = () => {
 	const [active, setActive] = useState('home')
 	const router = useRouter()
 	const pathname = usePathname()
+	const [open, setOpen] = useState(false)
+	const labelProps = {
+		variant: 'small',
+		className:
+			'absolute top-2/4 -left-2/4 -translate-y-2/4 -translate-x-3/4 font-normal bg-black-default p-1 text-white',
+	}
 
 	const handleClick = (name: string) => {
 		router.push(`/${name === 'home' ? '' : name}`, { scroll: false })
@@ -29,13 +44,15 @@ const FloatingMenu = () => {
 			setActive(pathname.replace('/', ''))
 		}
 	}, [pathname])
+
 	return (
-		<SpeedDial className='bg-white'>
+		<SpeedDial open={open} className='bg-white'>
 			<SpeedDialHandler>
 				<IconButton
+					onClick={() => setOpen(!open)}
 					placeholder={''}
 					size='lg'
-					className='rounded-full bg-secondary bg-opacity-50 text-black transition-colors hover:bg-opacity-100 group-hover:bg-opacity-100 group-hover:text-white'
+					className='rounded-full !bg-secondary text-black focus:!bg-secondary  '
 				>
 					<PlusIcon className='h-5 w-5 transition-transform group-hover:rotate-45' />
 				</IconButton>
@@ -47,6 +64,9 @@ const FloatingMenu = () => {
 					placeholder={undefined}
 				>
 					<HomeIcon className='h-5 w-5' />
+					<Typography placeholder={''} {...labelProps}>
+						Home
+					</Typography>
 				</SpeedDialAction>
 				<SpeedDialAction
 					className={active === 'projects' ? 'bg-secondary text-black' : ''}
@@ -54,9 +74,33 @@ const FloatingMenu = () => {
 					placeholder={undefined}
 				>
 					<AppWindow className='h-5 w-5' />
+					<Typography placeholder={''} {...labelProps}>
+						Projects
+					</Typography>
+				</SpeedDialAction>
+				<SpeedDialAction
+					onClick={() => handleClick('contact')}
+					placeholder={undefined}
+				>
+					<Contact className='h-5 w-5' />
+					<Typography placeholder={''} {...labelProps}>
+						Contact
+					</Typography>
+				</SpeedDialAction>
+				<SpeedDialAction
+					onClick={() => handleClick('blogs')}
+					placeholder={undefined}
+				>
+					<Rss className='h-5 w-5' />
+					<Typography placeholder={''} {...labelProps}>
+						Blogs
+					</Typography>
 				</SpeedDialAction>
 				<SpeedDialAction placeholder={undefined}>
-					<Github className='h-5 w-5' />
+					<Save className='h-5 w-5' />
+					<Typography placeholder={''} {...labelProps}>
+						Download Resume
+					</Typography>
 				</SpeedDialAction>
 			</SpeedDialContent>
 		</SpeedDial>
